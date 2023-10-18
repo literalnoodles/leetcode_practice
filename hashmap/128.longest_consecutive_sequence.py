ad8d1591d62d4c90baac7612e59c83743edff6f9
literalnoodles <trungnv.work@gmail.com>
2023-10-19 00:11:34 +0700

    231018

--- hashmap/128.longest_consecutive_sequence.py
+++ hashmap/128.longest_consecutive_sequence.py
@@ -6,7 +6,15 @@ from typing import List
 
 
 def longestConsecutive(nums: List[int]) -> int:
-    pass
+    nums_set = set(nums)
+    longest = 0
+    for num in nums:
+        if (num-1) not in nums_set:
+            i = 1
+            while (num + i) in nums_set:
+                i += 1
+            longest = max(longest, i)
+    return longest
 
 nums = [100,4,200,1,3,2]
 assert longestConsecutive(nums) == 4
